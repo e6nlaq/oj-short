f0ef281176a3411c7fb0d53c8b50c529ec821613
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2024-04-01 16:30:54 +0900

    add node

--- src/get.ts
+++ src/get.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Command } from '@commander-js/extra-typings';
 import chalk from 'chalk';
 import { rmSync } from 'fs';
--- src/subm.ts
+++ src/subm.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Command } from '@commander-js/extra-typings';
 
 import { load_config, RunConfig } from './base/config';
