97c8f5d100f796a38142c61ab58f42035bd84792
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-11-02 18:48:44 +0900

    chore: update build script

--- build.ts
+++ build.ts
@@ -9,11 +9,16 @@ const targets: Bun.Build.Target[] = [
 
 await $`rm -rf ./bin`;
 
+async function createZip(target: string) {
+    await $`zip -r ./bin/${target}.zip ./bin/out/* -j -9`;
+    console.log(`Created ${target}.zip`);
+}
+
 for (const target of targets) {
     await Bun.build({
         compile: {
             target,
-            outfile: `./bin/get-${target}`,
+            outfile: `./bin/out/get`,
         },
         entrypoints: ["src/get.ts"],
         minify: true,
@@ -21,9 +26,10 @@ for (const target of targets) {
     await Bun.build({
         compile: {
             target,
-            outfile: `./bin/subm-${target}`,
+            outfile: `./bin/out/subm`,
         },
         entrypoints: ["src/subm.ts"],
         minify: true,
     });
+    await createZip(target);
 }
