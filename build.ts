1b1f0b11c827ae85af44ccbdaa13e6732c78ab8e
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-11-02 12:40:20 +0900

    refactor: format

---
+++ build.ts
@@ -0,0 +1,25 @@
+const targets: Bun.Build.Target[] = [
+    "bun-linux-x64",
+    // "bun-linux-arm64",
+    // "bun-darwin-x64",
+    // "bun-darwin-arm64",
+];
+
+for (const target of targets) {
+    await Bun.build({
+        compile: {
+            target,
+            outfile: `./bin/get-${target}`,
+        },
+        entrypoints: ["src/get.ts"],
+    });
+    await Bun.build({
+        compile: {
+            target,
+            outfile: `./bin/subm-${target}`,
+        },
+        entrypoints: ["src/subm.ts"],
+    });
+}
+
+export {};
